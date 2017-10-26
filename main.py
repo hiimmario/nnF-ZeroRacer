215a5200c80795e47b9907a8bbe35a6311cc3653
hiimmario <m.reichart@hotmail.com>
2017-10-26 15:10:23 +0200

    start collecting first training data

--- main.py
+++ main.py
@@ -1,111 +1,61 @@
-from PIL import ImageGrab
-import numpy as np
 import cv2
 import time
-from draw_lanes import draw_lanes
+import numpy as np
 from grabscreen import grab_screen
-from directkeys import ReleaseKey, PressKey, W, A, S, D
+from getkeys import key_check
+import os
 
-def roi(img, vertices):
-    # blank mask:
-    mask = np.zeros_like(img)
+from getkeys import key_check
 
-    # filling pixels inside the polygon defined by "vertices" with the fill color
-    cv2.fillPoly(mask, vertices, 255)
+def keys_to_output(keys):
+    #[A, W, D]
+    output = [0, 0, 0]
 
-    # returning the image only where mask pixels are nonzero
-    masked = cv2.bitwise_and(img, mask)
-    return masked
+    if 'A'in keys:
+        output[0] = 1
+    if 'D'in keys:
+        output[2] = 1
+    else:
+        output[1] = 1
 
+    return output
 
-def process_img(image):
-    original_image = image
-    # convert to gray
-    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # edge detection
-    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
-
-    processed_img = cv2.GaussianBlur(processed_img, (5, 5), 0)
-
-    vertices = np.array([[0, 120], [346, 120], [346, 324], [0, 324]], np.int32)
-
-    processed_img = roi(processed_img, [vertices])
-
-    # more info: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
-    #                                     rho   theta   thresh  min length, max gap:
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi / 180, 180, np.array([]), 10, 3)
+file_name = 'data/training_data.npy'
 
 
-    m1 = 0
-    m2 = 0
-    '''
-    try:
-        l1, l2, m1, m2 = draw_lanes(original_image, lines)
-        cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [255, 0, 0], 5)
-        cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [255, 0, 0], 5)
-    except Exception as e:
-        print(str(e)) 
-        pass
-    try:
-        for coords in lines:
-            coords = coords[0]
-            try:
-                cv2.line(processed_img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 0, 0], 10)
+if os.path.isfile(file_name):
+    print('File exists, loading previous data!')
+    training_data = list(np.load(file_name))
+else:
+    print('File does not exist, starting fresh!')
+    training_data = []
 
 
-            except Exception as e:
-                print(str(e))
-    except Exception as e:
-        pass
-    
-    '''
-
-    return processed_img, original_image, m1, m2
-
-def straight():
-    PressKey(W)
-    ReleaseKey(A)
-    ReleaseKey(D)
-
-def left():
-    PressKey(A)
-    ReleaseKey(W)
-    ReleaseKey(D)
-    ReleaseKey(A)
-
-def right():
-    PressKey(D)
-    ReleaseKey(A)
-    ReleaseKey(W)
-    ReleaseKey(D)
-
-def slow_ya_roll():
-    ReleaseKey(W)
-    ReleaseKey(A)
-    ReleaseKey(D)
 
 def main():
     last_time = time.time()
     while True:
         screen = grab_screen(region=(100, 100, 348, 324))
-        print('Frame took {} seconds'.format(time.time() - last_time))
-        last_time = time.time()
-        new_screen, original_image, m1, m2 = process_img(screen)
+        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
 
-        cv2.imshow('window', new_screen)
-        cv2.imshow('window2', cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
-        cv2.imshow('window3', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         '''
-        if m1 < 0 and m2 < 0:
-            right()
-        elif m1 > 0 and m2 > 0:
-            left()
-        else:
-            straight()
-        '''
+        cv2.imshow('window', screen)          
+        
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
+        '''
+        screen = cv2.resize(screen, (160, 120))
+        keys = key_check()
+        output = keys_to_output(keys)
+        training_data.append([screen, output])
+
+        print('Frame took {} seconds'.format(time.time() - last_time))
+        last_time = time.time()
+
+        if len(training_data) % 500 == 0:
+            print(len(training_data))
+            np.save(file_name, training_data)
 
 for i in list(range(3))[::-1]:
     print(i+1)
